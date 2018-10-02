551875ed95db727167fa0c0d736f7359ab452057
Sajmon25 <szymon.grzedi@gmail.com>
2018-10-02 22:29:59 +0200

    Add database config file

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, Response, json
 
-app = Flask(__name__)
-
+# app = Flask(__name__)
+from settings import *
 books = [
     {
         'name': 'Green Egg and Ham',
